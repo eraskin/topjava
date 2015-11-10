94930e494f647375c5b1daddaeb75817062bae17
Alexei Eraskin <levrun1@gmail.com>
2015-11-11 00:30:37 +0100

    home work 0 - fist version

--- src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
+++ src/main/java/ru/javawebinar/topjava/model/UserMealWithExceed.java
@@ -1,6 +1,9 @@
 package ru.javawebinar.topjava.model;
 
+import ru.javawebinar.topjava.util.TimeUtil;
+
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 /**
  * GKislin
@@ -15,10 +18,21 @@ public class UserMealWithExceed {
 
     protected final boolean exceed;
 
+
+
     public UserMealWithExceed(LocalDateTime dateTime, String description, int calories, boolean exceed) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
         this.exceed = exceed;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public String toString() {
+        String exceedMessage = exceed ? " Ты слишком много съел в этот день" : "Как ты выдержал?! Удивительно!";
+        return description + " with " + calories + " calories from " + dateTime.format(TimeUtil.dateTimeFormatter) + " " + exceedMessage;
     }
 }
--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -1,12 +1,15 @@
 package ru.javawebinar.topjava.util;
 
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * GKislin
  * 07.01.2015.
  */
 public class TimeUtil {
+    public static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm");
+
     public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
     }
